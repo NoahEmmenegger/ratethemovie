06db502b698b0ed4dd73c51e404f1acc4e8c5a34
Noah Emmenegger <noahemmenegger.business@gmail.com>
2019-12-30 15:52:12 +0100

    add db connection

--- Classes/View/template/index.php
+++ Classes/View/template/index.php
@@ -1,3 +1,9 @@
+<?php
+    $con = mysqli_connect('localhost', 'root', '', 'ratethemovie');
+    $sql = "SELECT * FROM `film`";
+    $result = mysqli_query($con, $sql);
+?>
+
 <div class="startseitenblock">
     <h1>ratethemovie.ch</h1>
     <h2>Hier kommt ein kleiner Text</h2>
@@ -7,4 +13,15 @@
 <div class="beliebtfeed">
     <h1>Beliebteste Filme</h1>
     <h2><?php echo $this->data['testkey'] ?></h2>
+    <?php 
+        while($rows = mysqli_fetch_assoc($result))
+        {
+    ?>
+        <div class="filmbox">
+            <?php echo $rows['Titel'] ?>
+        </div>
+
+    <?php
+        }
+    ?>
 </div>
