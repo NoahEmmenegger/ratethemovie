3adf2859be753cd523726b9e87fa3c7e7b53938f
Noah Emmenegger <noahemmenegger.business@gmail.com>
2020-01-11 13:43:17 +0100

    add star rating

--- Classes/View/template/detail.php
+++ Classes/View/template/detail.php
@@ -1,5 +1,34 @@
 <div class="FilmDetail">
-    <?php echo $this->data['anzahlSterneFilm'] ?>
+    <div class="stars">
+    <?php
+    $x = 1;
+        while ($x <= 5)
+        {
+            if($x <= $this->data['anzahlSterneFilm'])
+            {
+                ?>
+                <span class="star on"></span>
+                <?php
+            }else {
+                if($x- 0.5 == $this->data['anzahlSterneFilm'])
+                {
+                    ?>
+                    <span class="star half"></span>
+                    <?php
+                } else {
+                ?>
+                <span class="star"></span>
+                <?php
+                }
+            }
+            $x++;
+    ?>
+    
+    <?php
+        }
+    ?>
+    </div>
+
     <h1 style="text-align: center"><?php echo $this->data['film']['Titel'] ?></h1>
     <h3 style="text-align: center">von <?php echo $this->data['film']['Produzent'] ?></h3>
     <img class="detailImage" src="http://localhost/ratethemovie/<?php echo $this->data['film']['Bild'] ?>"/>
@@ -45,7 +74,35 @@
         <div class="kommentar">
             <div>
                 <h1 style="display: inline-block"><?php echo $kommentar['Vorname'] . ' ' . $kommentar['Nachname'] ?></h1>
-                <small>Anzahl Sterne (<?php echo $kommentar['AnzahlSterne'] ?>)</small>
+                <div class="stars" style="display: inline-block; float:right">
+    <?php
+    $x = 1;
+        while ($x <= 5)
+        {
+            if($x <= $kommentar['AnzahlSterne'])
+            {
+                ?>
+                <span class="star on"></span>
+                <?php
+            }else {
+                if($x- 0.5 == $kommentar['AnzahlSterne'])
+                {
+                    ?>
+                    <span class="star half"></span>
+                    <?php
+                } else {
+                ?>
+                <span class="star"></span>
+                <?php
+                }
+            }
+            $x++;
+    ?>
+    
+    <?php
+        }
+    ?>
+    </div>
             </div>
             <h3><?php echo $kommentar['Inhalt'] ?></h1>
         </div>
